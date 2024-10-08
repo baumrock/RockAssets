7aec2cc33b617c6cc608033aac85e4f19cd54f8b
BernhardBaumrock <office@baumrock.com>
2024-10-08 14:02:01 +0200

    add summary

--- RockAssets.info.php
+++ RockAssets.info.php
@@ -5,7 +5,7 @@ namespace ProcessWire;
 $info = [
   'title' => 'RockAssets',
   'version' => json_decode(file_get_contents(__DIR__ . "/package.json"))->version,
-  'summary' => '',
+  'summary' => 'Merge and minify LESS/SCSS/CSS/JS files via PHP',
   'autoload' => false,
   'singular' => false,
   'icon' => 'code',
